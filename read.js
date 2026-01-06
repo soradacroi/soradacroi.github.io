7564df0347d70f56c9b3c18123ed45f3b2db4036
soradacroi <soradacroi@gmail.com>
2026-01-06 23:01:09 +0530

    spelled work incorrectly man...

--- read.js
+++ read.js
@@ -1,7 +1,7 @@
 
 async function loadText() {
   try {
-    const response = await fetch('wprk.txt');
+    const response = await fetch('work.txt');
     const text = await response.text();
     
     document.getElementById('content-target').textContent = text;
