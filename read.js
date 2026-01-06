86bf2f3e4c6b6f3b672118aea38ad94e544b50e8
soradacroi <soradacroi@gmail.com>
2026-01-06 22:57:38 +0530

    file reading

---
+++ read.js
@@ -0,0 +1,14 @@
+
+async function loadText() {
+  try {
+    const response = await fetch('wprk.txt');
+    const text = await response.text();
+    
+    document.getElementById('content-target').textContent = text;
+  } catch (error) {
+    console.error("Error loading the text file:", error);
+    document.getElementById('content-target').textContent = "Failed to load file.";
+  }
+}
+
+loadText();
