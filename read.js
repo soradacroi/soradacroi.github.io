42ff5a765ff40d3d72b4aa998686e87c25ed9f35
soradacroi <soradacroi@gmail.com>
2026-01-06 23:53:05 +0530

    shows the project post

--- read.js
+++ read.js
@@ -4,19 +4,33 @@ async function loadText() {
     const fullText = await response.text();
 
     const lines = fullText.split(/\r?\n/);
-
     const listContainer = document.getElementById('content-list');
-    listContainer.innerHTML = '';
+    
+    if (listContainer) {
+      listContainer.innerHTML = '';
 
-    lines.forEach((line, index) => {
-      
-      const li = document.createElement('li');
-      li.textContent = line;
-      listContainer.appendChild(li);
-    });
+      lines.forEach((line) => {
+        if (line.trim() === "") return;
 
+        const parts = line.split('|');
+        const siteName = parts[0].trim();
+        const siteUrl = parts[1] ? parts[1].trim() : "#";
+        const li = document.createElement('li');
+        
+
+        const link = document.createElement('a');
+        link.textContent = siteName;
+        link.href = siteUrl;
+        link.target = "_blank";
+        link.className = "link"; 
+
+        li.appendChild(link);
+        listContainer.appendChild(li); 
+      });
+    }
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Error:", error); 
   }
 }
+
 loadText();
